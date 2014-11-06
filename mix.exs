980c2f7d6ad75baddd3d957b695a90ab9ca061be
Michael Chmielewski <code@mikec.123mail.org>
2014-11-05 20:53:50 -0500

    Add dicer as application to make sure exrm sees it during build

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule Infiniteroll.Mixfile do
   # Type `mix help compile.app` for more information
   def application do
     [mod: {Infiniteroll, []},
-     applications: [:phoenix, :cowboy, :logger]]
+     applications: [:dicer, :phoenix, :cowboy, :logger]]
   end
 
   # Specifies your project dependencies
